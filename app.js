471e8dd321d4293b405dfcb97a3f462c46801e01
rajeswari <rajeswari.elogic@gmail.com>
2020-09-08 12:33:38 +0530

    customfilters

--- app.js
+++ app.js
@@ -2,24 +2,32 @@
 'use strict';
 angular.module('nameApp', [])
 .controller('nameAppController', nameAppController)
-.filter('loves', LovesFilter);
+.filter('loves', LovesFilter)
+.filter('truth', truthFilter);
 nameAppController.$inject = ['$scope', 'lovesFilter'];
 function nameAppController($scope, lovesFilter){
 
 $scope.sayHello = function(){
-    var msg = "Rajee likes to eat fruits";
+    var msg = "Rajee likes to eat healthy snacks";
     return msg;
 };
 $scope.sayLovesHello = function(){
-    var msg = "Rajee likes to eat fruits";
+    var msg = "Rajee likes to eat healthy snacks";
     msg = lovesFilter(msg);
-    return msg;
+    return msg; 
 };
 }
 function LovesFilter(){
     return function(input){
         input = input || "";
         input = input.replace("likes","loves");
+        return input;
+    };
+}
+function truthFilter(){
+    return function(input, target, replace){
+        input = input || "";
+        input = input.replace(target, replace);
         return input;
     };
 }
